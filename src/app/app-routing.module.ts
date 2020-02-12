a59dedb3c4c4741b29ff99676d876ced64830b3f
derek white <derekbwhite80@gmail.com>
2020-02-12 10:54:58 -0500

    add profile path to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Profile } from './all-files/profile/profile';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', component: Profile }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
